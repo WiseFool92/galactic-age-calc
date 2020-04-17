8fa45aeb427b91188b46a09c2506eb422e64ece5
Nathan Watkins Hoagland <watkins92@gmail.com>
2020-04-17 10:32:39 -0700

    constant variable multipliers for each planet, class wrap w/construtors & earthAge method

---
+++ src/sagansDot.js
@@ -0,0 +1,12 @@
+// variable multipliers for each planet
+const mercurianAge = 0.24;
+const venitianAge = 0.62;
+const martianAge = 1.88;
+const jovianAge = 11.86;
+
+// Class object that will wrap around all our functions
+export class Sagan {
+  constructor() {
+    
+  }
+}
